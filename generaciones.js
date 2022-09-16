ec3478ff7c2f2f37edd9071eeb374d2ecfc46218
josetrivino89 <josetrivino89@gmail.com>
2022-09-15 22:45:04 -0300

    se reformulo ejercicio 2 , se agrega localStorage y funcion para incorporar lineas a tablas

--- generaciones.js
+++ generaciones.js
@@ -58,46 +58,123 @@
 
 // let persona1=[]
 // console.log(persona1)
+    // let Nombre = (document.getElementById("Nombre").value);
+    // let edad = (document.getElementById("edad").value);
+    // let dni  = (document.getElementById("DNI").value);
+    // let Sexo  = (document.getElementById("Sexo").value);
+    // let Peso  = (document.getElementById("peso").value);
+    // let altura  = (document.getElementById("altura").value);    
+    // let anioNacimiento  = (document.getElementById("anioNacimiento").value);
+    // let form=document.getElementById("form")
+    // console.log(form)
+    
+
+     //creo el array donde voy a guardar las personas, uso JSon para traer de LocalStorage
+    let ArrNuevaPersona = JSON.parse(localStorage.getItem("Lista_Personas")) || [];
+
+    //creo una funcion para crear objeto persona y pushar al array
+    const handleSubmit=(e)=>{
+        // PARA EVITAR QUE ACTUALICE
+        e.preventDefault();
+        //TRAER LOS DATOS DEL INPUT
+        let Nombre = (document.getElementById("Nombre").value);
+        let edad = (document.getElementById("edad").value);
+        let dni  = (document.getElementById("DNI").value);
+        let Sexo  = (document.getElementById("Sexo").value);
+        let Peso  = (document.getElementById("peso").value);
+        let altura  = (document.getElementById("altura").value);    
+        let anioNacimiento  = (document.getElementById("anioNacimiento").value);
+        //CREA LA NUEVA PERSONA
+        const nuevaPersona = new Personas(Nombre,edad,dni,Sexo,Peso,altura,anioNacimiento)
+        //MUESTRA POR PANTALLA
+        console.log(nuevaPersona);
+        //MANDO LOS DATOS AL ARRAY
+        ArrNuevaPersona.push(nuevaPersona);
+        //MUESTO POR PANTALL
+        console.log(ArrNuevaPersona);
+        //ENVIO LA INFO A LOCASTORAGE
+        localStorage.setItem("Lista_Personas",JSON.stringify(ArrNuevaPersona));
+        //RESETEO EL FORMULARIO
+        form.reset()
+        //RECARGO LA PAGINA
+        window.location.reload() 
+    }
+    form.addEventListener("submit",handleSubmit)
+
+    
+let consultaLS = JSON.parse(localStorage.getItem("Lista_Personas"))|| [];
+console.log(consultaLS)
+
+    //creo funcion para ir agregando los nombre:  
+     let listaPersonas = document.getElementById("ListaPersonas");
+     console.log(listaPersonas);
+
+     const createPersonas=(persona)=>{
+        ListaPersonas.innerHTML +=`<th class=" text-center">${persona.Nombre}</th>
+         <th class=" text-center">${persona.edad}</th>
+         <th class=" text-center">${persona.dni}</th>
+         <th class=" text-center">${persona.Sexo}</th>
+         <th class=" text-center">${persona.Peso}</th>
+         <th class=" text-center">${persona.altura}</th>
+         <th class=" text-center">${persona.anioNacimiento}</th>
+         <th class="text-center" >
+                            <button class=" btn btn-danger">generacion</button>
+                            <button class=" btn btn-warning">Mayoria edad</button>
+                            <button class=" btn btn-info">Mis Datos</button>
+        </th>`
+        
+        
+
+     }
+     ///creo un for para recorrer la funcion, esto entra al array y va ejecutando la funcion "create personas"
+     for (let index = 0; index < consultaLS.length; index++) {
+        const element = consultaLS[index];
+       createPersonas(element);
+        
+        
+     } 
+      
+    
  
-function optenerValores() {
-    let Nombre = (document.getElementById("Nombre").value);
-    let edad = (document.getElementById("edad").value);
-    let dni  = (document.getElementById("DNI").value);
-    let Sexo  = (document.getElementById("Sexo").value);
-    let Peso  = (document.getElementById("peso").value);
-    let altura  = (document.getElementById("altura").value);    
-    let anioNacimiento  = (document.getElementById("anioNacimiento").value);
-    // persona1.push(Nombre,dni,Sexo,Peso,altura,anioNacimiento);
-    let persona = new Personas(Nombre,edad,dni,Sexo,Peso,altura,anioNacimiento)
-    console.log(persona);
-    persona.mostrarGeneracion()
+ function optenerValores() {
+     let Nombre = (document.getElementById("Nombre").value);
+     let edad = (document.getElementById("edad").value);
+     let dni  = (document.getElementById("DNI").value);
+     let Sexo  = (document.getElementById("Sexo").value);
+     let Peso  = (document.getElementById("peso").value);
+     let altura  = (document.getElementById("altura").value);    
+     let anioNacimiento  = (document.getElementById("anioNacimiento").value);
+     // persona1.push(Nombre,dni,Sexo,Peso,altura,anioNacimiento);
+     let persona = new Personas(Nombre,edad,dni,Sexo,Peso,altura,anioNacimiento)
+     console.log(persona);
+     persona.mostrarGeneracion()
 
 }
 
-function mayoriaEdad() {
-    let Nombre = (document.getElementById("Nombre").value);
-    let edad = (document.getElementById("edad").value);
-    let dni  = (document.getElementById("DNI").value);
-    let Sexo  = (document.getElementById("Sexo").value);
-    let Peso  = (document.getElementById("peso").value);
-    let altura  = (document.getElementById("altura").value);    
-    let anioNacimiento  = (document.getElementById("anioNacimiento").value);
-    // persona1.push(Nombre,dni,Sexo,Peso,altura,anioNacimiento);
-    let persona = new Personas(Nombre,edad,dni,Sexo,Peso,altura,anioNacimiento)
-    console.log(persona);
-    persona.esMayorDeEdadd()
+// function mayoriaEdad() {
+//     let Nombre = (document.getElementById("Nombre").value);
+//     let edad = (document.getElementById("edad").value);
+//     let dni  = (document.getElementById("DNI").value);
+//     let Sexo  = (document.getElementById("Sexo").value);
+//     let Peso  = (document.getElementById("peso").value);
+//     let altura  = (document.getElementById("altura").value);    
+//     let anioNacimiento  = (document.getElementById("anioNacimiento").value);
+//     // persona1.push(Nombre,dni,Sexo,Peso,altura,anioNacimiento);
+//     let persona = new Personas(Nombre,edad,dni,Sexo,Peso,altura,anioNacimiento)
+//     console.log(persona);
+//     persona.esMayorDeEdadd()
 
-}
-function mostrarDatosUsuario() {
-    let Nombre = (document.getElementById("Nombre").value);
-    let edad = (document.getElementById("edad").value);
-    let dni  = (document.getElementById("DNI").value);
-    let Sexo  = (document.getElementById("Sexo").value);
-    let Peso  = (document.getElementById("peso").value);
-    let altura  = (document.getElementById("altura").value);    
-    let anioNacimiento  = (document.getElementById("anioNacimiento").value);
-    // persona1.push(Nombre,dni,Sexo,Peso,altura,anioNacimiento);
-    let persona = new Personas(Nombre,edad,dni,Sexo,Peso,altura,anioNacimiento)
-    console.log(persona);
-    persona.mostrarDatos()
-}
+// }
+// function mostrarDatosUsuario() {
+//     let Nombre = (document.getElementById("Nombre").value);
+//     let edad = (document.getElementById("edad").value);
+//     let dni  = (document.getElementById("DNI").value);
+//     let Sexo  = (document.getElementById("Sexo").value);
+//     let Peso  = (document.getElementById("peso").value);
+//     let altura  = (document.getElementById("altura").value);    
+//     let anioNacimiento  = (document.getElementById("anioNacimiento").value);
+//     // persona1.push(Nombre,dni,Sexo,Peso,altura,anioNacimiento);
+//     let persona = new Personas(Nombre,edad,dni,Sexo,Peso,altura,anioNacimiento)
+//     console.log(persona);
+//     persona.mostrarDatos()
+// }
